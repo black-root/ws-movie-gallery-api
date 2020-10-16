dacef6ff56285f838429e7d1a1ee338141aefebe
Ronald Ibanez <ronald.ibanez1994@gmail.com>
2020-10-15 22:16:01 -0600

    Everyone (authenticated or not) can get the detail of a movie.

--- src/main/java/com/blackroot/movie/gallery/api/config/SecurityConfig.java
+++ src/main/java/com/blackroot/movie/gallery/api/config/SecurityConfig.java
@@ -29,7 +29,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     private static final String[] AUTH_WHITELIST = {
 			"/authenticate",
-			"/movies"
+			"/movies",
+			"/movies/findByTitle/*"
 	};
 
     @Override
